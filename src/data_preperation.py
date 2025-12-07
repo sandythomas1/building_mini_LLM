8184a7308507247efd274b09ab44d83f3034e331
Sandy Thomas <sandy.thomas@calbaptist.edu>
2025-12-06 16:38:00 -0800

    Update data_preperation.py

--- src/data_preperation.py
+++ src/data_preperation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72d7f17d187e48a3eecb7ea16b311e6aa41540d0eae7116af817573fa10c05b2
-size 272
+oid sha256:7f4ed767d6d14f5ec38a9a9109fd0f67c0708f98452e793f0dc0c4657b4a6752
+size 271
